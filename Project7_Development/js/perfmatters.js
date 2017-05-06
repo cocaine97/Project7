ff2cafa9dbf04f2282823df9c596a41a6864e553
Cocaine97 <cocaine31197@gmail.com>
2017-05-07 00:00:56 +0530

    did the gruntwork and polish

---
+++ Project7_Development/js/perfmatters.js
@@ -0,0 +1,5 @@
+function logCRP() {
+    vart: window.performance.timing,
+    dcl = t.domContentLoadedEventStart - t.domLoading,
+    complete = t.domComplete - t.domLoading;varstats: document.getElementById(crp - stats);statstextcontent: 'DCL: ' + dcl + 'ms,onload: ' + complete + ms
+}
--- js/perfmatters.js
+++
@@ -1,1 +0,0 @@
-function logCRP(){vart:window.performance.timing,dcl = t.domContentLoadedEventStart - t.domLoading,complete = t.domComplete - t.domLoading;varstats:document.getElementById(crp-stats);statstextcontent:'DCL: ' + dcl + 'ms,onload: ' + complete + ms}
